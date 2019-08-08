ffff97127bb48eb95ef4f9a71d8413719f4c5a46
L <karmameet@gmail.com>
2019-08-08 10:59:14 +0800

    Update cdcode.sh
    移除特殊环境、特殊目录的限制

--- 7-cdcode/cdcode.sh
+++ 7-cdcode/cdcode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# 该脚本用于在exdroid和lichee的各个目录之间快速cd.把各个路径和一些简单的
+# 该脚本用于在Android源码下面的各个目录之间快速cd.把各个路径和一些简单的
 # 字母或数字对应起来,通过输入字母或数字来快速cd到那个路径,减少键盘输入.
-# 例如执行 cdcode.sh b 会 cd 到 exdroid/framework/base 目录等.
+# 例如执行 cdcode.sh b 会 cd 到 framework/base 目录,或者配置的其他目录下,
 # 另外,当一个路径简写以字符'+'开头时,表示进入该简写对应的目录下进行编译.
 
 # 注意,在脚本内执行cd命令,当脚本退出时,不会保持在cd后的路径,而是恢复
@@ -14,31 +14,22 @@
 # alias c='source ~/bin/cdcode.sh' # 注意单引号必不可少,也可改成双引号
 
 # 脚本保存的路径简写及其对应路径的配置文件
-DIRINFO="${HOME}/.myconf/dirinfo.txt"
-# 保存 Android 目录下全局文件路径名的配置文件
-LISTFILE="listfiles"
+DIRINFO="${HOME}/.liconfig/dirinfo.txt"
 
 # 当一个路径简写以字符'+'开头时,表示进入该简写对应的目录下进行编译.
 COMPILE_PREFIX="+"
-
-# 指定编译 Android 时要 lunch 的分支名
-BUILD_ENG="ococci-eng"
-BUILD_USER="ococci-user"
-
-# 当 search_files 变量等于1时,将会调用 godir() 函数进行全局查找.
-# 一旦将这个变量设置为 1, 会在脚本执行期间一直生效.
-declare search_files=0
+# 指定编译 Android 时默认要 lunch 的分支名
+# 将DEFAULT_LAUNCH_COMBO修改成要编译的project名称
+DEFAULT_LAUNCH_COMBO="xxxxx-userdebug"
 
 # 该变量保存代码根目录的完整路径,后续作为要cd到的路径的一部分.
 # !!注意!!: 不要在脚本开头为这个变量赋初值!每次 cd 时,都会执行这个脚本,但
 # 不是每次 cd 都会设置代码根目录.在多次调用该脚本时,要保持该变量的值不变.
 # declare project_root_dir=""
 
-# 导入Android相关脚本的通用常量.
+# 导入Android相关的常量和函数
 source android.sh
-# 导入 cd_project_root() 函数的定义.
-source procommon.sh
-# 导入解析配置文件的脚本.要先定义 DIRINFO 变量的值,在 source 该脚本.
+# 导入解析配置文件的脚本.要先定义 DIRINFO 变量的值,再 source 该脚本.
 source parsecfg.sh "${DIRINFO}"
 # 当 parsecfg.sh 解析配置文件失败时,则退出,不再往下执行.
 if [ $? -ne 0 ]; then
@@ -48,50 +39,32 @@ fi
 cdcode_show_help()
 {
 printf "USAGE
-        cdcode.sh [option] symbol
+        cdcode.sh [option] [symbol]
 OPTIONS
-        symbol: 要 cd 到的路径简写,支持的简写可以用-l或-v选项来查看.
-                当一个路径简写以字符'+'开头时,表示进入该路径简写对应
-                的目录下进行编译.
         option: 可选选项,描述如下:
             -h: 打印这个帮助信息.
-            -f: 如果找不到所给的symbol,则进行全局文件搜索.
+            -f: 在Android源码下查找指定的文件,需要一个参数来提供文件名.
             -p: 打印当前所在的代码根目录
             -r: 设置代码切换的根目录,需要一个参数,来指定代码的根目录路径.
             -s: 进行source并lunch的操作,但是不进行编译.
-            -n: 先编译lichee,再编译android.
             -m: 在Android根目录下,进行android全编译.
-            -x: 在lichee根目录下,进行lichee全编译.
-            -g: 编译android gms固件.
             -l: 查看脚本支持的路径简写及其对应的路径.
             -v: 以键值对的方式打印详细的配置文件信息.
             -i: 在配置文件中查找指定内容.需要一个参数来指定要查找的内容.
             -e: 使用vim打开脚本的配置文件,以供编辑.
             -a: 增加或修改一个路径简写和对应的路径到配置文件.需要一个参数, 
                 用单引号括起来,以指定路径简写和路径.格式为: 路径简写|路径
-                例如 -a 'p|exdroid/packages/apps/Phone',如果 p 简写还不存
-                在新增它,否则修改它.注意:路径要求以exdroid/或者lichee/开头.
+                例如: -a 'p|packages/apps/Phone',如果 p 简写还不存在会新增
+                这个配置项,否则修改 p 简写对应的路径为新的路径.
             -d: 从脚本配置文件中删除一个路径简写和对应的路径.需要一个
                 参数,以指定路径简写,脚本将从配置文件中删除该路径简写
                 对应的行. 例如 -d a, 将删除路径简写为 a 的行.
+        symbol: 可选选项,指定要 cd 到的路径简写,有效的简写可以用-l或-v选项来查看.
+                当一个路径简写以字符'+'开头时,表示进入该路径简写对应
+                的目录下进行编译.
+        当不带任何参数且已经设置过代码根目录时,该脚本会cd到代码根目录下.
 "
 }
-
-# 检查传入的路径是否以exdroid/或者lichee/开头,如果是,返回0,否则返回1.
-# 这个函数主要是防止传入的路径不符合要求,目前不需要,先注释,要用时再打开.
-# check_pathheader()
-# {
-#     local head path
-# 
-#     path="$1"
-#     head="${path%%/*}"
-#     # expr1 -a expr2 表示执行一个与运算,要求这两个表达式都为真
-#     if [ "${head}" != "${EXDROID}" -a "${head}" != "${LICHEE}" ]; then
-#         echo "出错,路径要求以exdroid/或者lichee/开头"
-#         return 1
-#     fi
-#     return 0
-# }
 
 show_project_root()
 {
@@ -106,64 +79,31 @@ setup_project_root()
     fi
 
     \cd "$1"
-    # 检查所设置的代码根目录下是否包含 "exdroid" 目录.如果包含,则这个
+    # 检查所设置的代码根目录下是否包含 "frameworks" 目录.如果包含,则这个
     # 代码根目录符合要求,否则不符合要求,不为project_root_dir变量赋值.
-    pwd_dir_check "${EXDROID}"
+    # 下面的pwd_check_sub_dir()函数和FRAMEWORK常量来自android.sh脚本.
+    pwd_check_sub_dir "${FRAMEWORKS}"
     if [ $? -eq 0 ]; then
-        project_root_dir=$(pwd)
-        EXDROID_ROOT_DIR=${project_root_dir}/${EXDROID}
-        LICHEE_ROOT_DIR=${project_root_dir}/${LICHEE}
+        project_root_dir="$(pwd)"
     fi
 }
 
-# 如果之间的project_root_dir变量是/home/work/john/7-sw-a31s-4.4,而当前位于
-# /home/work/john/6-sw-a31s-4.4-sdk4.6/目录下,那么需要更新代码根目录为当前
-# 目录.一般而言,在当前目录下执行该脚本,是希望在当前目录下cd,如果不更新代码
-# 根目录,则在目录6-sw-a31s-4.4-sdk4.6下执行该脚本,可能会cd到7-sw-a31s-4.4
-# 目录下.此时,如果没有察觉到这一点,所做的改动就没有发生在预期的目录下.
-# 如果当前目录路径中不包含lichee或者exdroid目录,则认为当前没有位于lichee或
-# 者exdroid代码之下,将不会更新代码根目录,默认使用当前的代码根目录路径.
-update_project_root()
+# 如果当前路径不位于代码根目录底下,打印一些提示信息,以便用户关注到这一点.
+# 例如,代码根目录是 code/android,而当前用户所在路径是 test/android,可能
+# 预期执行该脚本时会 cd 到 test/android/ 目录下的子目录,但实际会 cd 到
+# code/android/ 目录下的子目录,就可能会修改到不预取目录的代码文件. 为了
+# 避免这个问题,所以要打印提示信息.
+# NOTE: 原本预期是可以自动更新代码根目录到当前路径下,但是目前无法通过路径
+# 名来判断当前路径是否包含Android源码.如果是假设Android源码放在android目录
+# 下,这会引入目录名的限制,目前不采用这种方法.
+check_project_root()
 {
-    local dirpath current_root
-
-    # 由于这个脚本会在"exdroid"和"lichee"目录之间相互切换,例如当前路径为
-    #   "/home/work/john/1-cs-a31s-4.2-x10-g3/lichee/linux-3.3/drivers"
-    # 这个路径不包含"exdroid",则后面的 ${dirpath%/exdroid*} 就匹配失败,
-    # 无法提取出正确的代码根目录.所以这里将当前路径中的"lichee"替换为
-    # "exdroid",以便能匹配成功.如果路径中不包含"lichee",则不进行替换,不会
-    # 造成影响.注意一下 pwd 和 PWD 的区别, pwd是shell命令,而 PWD是shell变
-    # 量,Bash的字符串操作要以shell变量作为参数,所以用PWD,不能用小写的pwd.
-    dirpath=${PWD/${LICHEE}/${EXDROID}}
-
-    # 判断替换后的目录路径中是否包含"exdroid",如果不包含,不更新代码根目录.
-    # 如果grep查找成功,会打印查找结果.为了不想显示查找结果,所以将它重定向.
-    echo ${dirpath} | grep ${EXDROID} > /dev/null
-    if [ $? -ne 0 ]; then
-        return $?
-    fi
-
-    # 使用字符串匹配从当前路径中提取位于出"/exdroid"之前的内容.假设路径是
-    #   "/home/work/john/6-sw-a31s-4.4-sdk4.6/exdroid/frameworks/base"
-    # 则 "/exdroid*" 匹配上面的 "/exdroid/frameworks/base",除去该匹配外,
-    # 剩余的内容是"/home/work/john/6-sw-a31s-4.4-sdk4.6",下面的字符串匹配
-    # 得到的内容就是这个值,可以用于和 project_root_dir 变量的值进行比较.
-    # current_root=${dirpath%/${EXDROID}*}
-    #
-    # 按照上面的语句会提取出"/exdroid"之前的内容,实际执行时遇过这样的情况:
-    # 有一个路径名是: /home/work/john/2-sw-a31s-4.4-mt915/sys-lichee
-    # 经过上面的替换后是: /home/work/john/2-sw-a31s-4.4-mt915/sys-exdroid
-    # 用 grep 命令来查找路径名是否带有"exdroid",此时是匹配的,又试图提取出
-    # "/exdroid"之前内容时,则提取不到. "sys-exdroid" 匹配 "/*exdroid*" 这
-    # 个模式,不匹配 "/exdroid*" 模式,所以下面将模式改成 "/*exdroid*".
-    current_root=${dirpath%/*${EXDROID}*}
-    if [ "${project_root_dir}" != "${current_root}" ]; then
-        echo "将代码根目录从 ${project_root_dir} 更新到 ${current_root}"
-        setup_project_root "${current_root}"
+    if [[ ! $(pwd) =~ "${project_root_dir}" ]]; then
+        echo -e 当前路径'\033[31m' $(pwd) '\033[0m'位于设置的代码根目录之外,
+        echo -e 将会跳转到'\033[32m' 代码根目录${project_root_dir} '\033[0m'底下的子目录!
     fi
 }
 
-# 强制在"exdroid"目录下 source 和 lunch,不判断是否已经source过.
 source_and_lunch()
 {
     # 如果 project_root_dir 变量为空,则还没有设置代码根目录,报错返回
@@ -173,13 +113,11 @@ source_and_lunch()
     fi
 
     if [ $# -ne 1 ]; then
-        echo "Usage: $FUNCNAME build_variant"
+        echo "Usage: $FUNCNAME launch_combo"
         return 1
     fi
 
-    # 指定要 cd 到的绝对路径,以便从Android代码的子目录跳转到"exdroid"目录
-    \cd "${EXDROID_ROOT_DIR}"
-
+    \cd "${project_root_dir}"
     source build/envsetup.sh
     # 在Android的"build/envsetup.sh"脚本中,有一个print_lunch_menu()函数可以
     # 打印所有lunch分支,格式类似于"1. aosp_arm-eng".其中,1 是lunch分支编号,
@@ -189,69 +127,42 @@ source_and_lunch()
     lunch $(print_lunch_menu | grep "${1}" | awk '{print $2}')
 }
 
-compile_lichee()
-{
-    \cd ${LICHEE_ROOT_DIR} && bulichee.sh
-    if [ $? -ne 0 ]; then
-        echo -e '\033[31m编译 lichee 出错!\033[0m'
-        return 1
-    fi
-}
-
 compile_android()
 {
-    source_and_lunch "${BUILD_ENG}" && \
-        extract-bsp && make -j8 && pack && pack -d
+    source_and_lunch "${DEFAULT_LAUNCH_COMBO}" && \
+        make -j16 2>&1 | tee build_android.log
     if [ $? -ne 0 ]; then
         echo -e '\033[31m编译 Android 出错!\033[0m'
         return 1
     fi
 }
 
-compile_gms()
-{
-    source_and_lunch "${BUILD_USER}" && extract-bsp && make -j8
-    if [ $? -ne 0 ]; then
-        get_uboot && make -j8 dist && pack -s && pack -d -s
-    else
-        echo -e '\033[31m编译GMS固件出错!\033[0m'
-        return 1
-    fi
-}
-
-compile_all()
-{
-    compile_lichee && compile_android
-    if [ $? -ne 0 ]; then
-        return 1
-    fi
-}
-
-# 保存Android源码各个目录和文件的名字到${LISTFILE}文件里面,然后可以在该
+# 保存Android源码各个目录和文件的名字到一个文本文件里面,然后可以在该
 # 文件中查找指定的文件名,如果只找到一个就直接 cd 到该文件所在的目录.如果
 # 找到多个匹配项,就弹出列表给用户选择,选择之后再 cd 过去.
+# 这个函数复制自Android源码的build/make/envsetup.sh文件,进行了一些修改.
 godir() {
     if [[ -z "$1" ]]; then
         echo "Usage: godir <regex>"
         return
     fi
-    local T="${project_root_dir}/${EXDROID}"
-    if [[ ! -f $T/${LISTFILE} ]]; then
+    local T="${project_root_dir}/"
+    local FILELIST="filelists"
+    if [[ ! -f $T/${FILELIST} ]]; then
         echo -n "Creating index..."
         \cd $T
-        find . \( -path "*test*" -o -path "*git*" -o -path "./out" \
-            -o -path "./cts" -o -path "./gdk" -o -path "./ndk" \
-            -o -path "./pdk" -o -path "./sdk" -o -path "./abi" \
-            -o -path "./art" -o -path "./.repo" -o -path "*docs" \
-            -o -path "*tools" -o -path "./libnativehelper" \
-            -o -path "./dalvik" -o -path "./prebuilt" \
-            -o -path "libcore" -o -path "./development" \) -prune \
-            -o -type f > ${LISTFILE}
+        find ./bionic ./build ./device ./external \
+            ./frameworks ./hardware ./kernel \
+            ./packages ./system ./vendor \
+            \( -path "*test*" -o -path "*tools" -o -path "*docs" \
+            -o -path "*git*" -o -path "*svn" \) -prune \
+            -o -print -o -type f \
+            | grep "\.[a-z]" | grep -v -E "\.html|\.txt" > $FILELIST
             # -o -regex '.*\.\(c\|xml\|cpp\|h\|java\|rc\|mk\|fex\)'
         echo " Done"
     fi
     local lines
-    lines=($(\grep "$1" $T/${LISTFILE}|sed -e 's/\/[^/]*$//'|sort|uniq))
+    lines=($(\grep "$1" $T/${FILELIST}|sed -e 's/\/[^/]*$//'|sort|uniq))
     if [[ ${#lines[@]} = 0 ]]; then
         echo "Not found"
         return
@@ -289,15 +200,15 @@ handle_symbol()
         echo "Usage: $FUNCNAME symbol"
         return 1
     fi
-    local key alif to_do dir_value
+    local key prefix mm_cmd dir_value
 
     key="$1"
     # 获取路径简写的首字母.如果首字母是'+',则进到该简写对应的目录下进行编译
     # 注意: 要先source之后,才能执行mm命令.为了避免多次source,不会在每次mm之
     # 前都执行一次source,所以要求先source,再执行这个编译命令,否则编译报错.
-    alif="${key:0:1}"
-    if [ "${alif}" == "${COMPILE_PREFIX}" ]; then
-        to_do="mm"
+    prefix="${key:0:1}"
+    if [ "${prefix}" == "${COMPILE_PREFIX}" ]; then
+        mm_cmd="mm"
         # 此时,需要去掉'+'这个首字母,该字母不属于路径简写的一部分.
         key=${key:1}
     fi
@@ -305,26 +216,33 @@ handle_symbol()
     dir_value=$(get_value_by_key "${key}")
     if [ $? -eq 0 ]; then
         \cd ${project_root_dir}/${dir_value}
-        ${to_do}
+        if [ -n "${mm_cmd}" ]; then
+            ${mm_cmd}
+        fi
         return 0
     fi
 
     echo "找不到路径简写 '${key}' 对应的行"
-    if [ ${search_files} -eq 1 ]; then
-        echo "尝试进行全局文件搜索 ..."
-        godir ${key}
-    fi
+    return 1
 }
 
 if [ $# -eq 0 ]; then
-    cdcode_show_help
-    return 1
+    if [ -z "${project_root_dir}" ]; then
+        # 如果 project_root_dir 变量为空,则还没有设置代码根目录,打印帮忙信息
+        echo "NOTE: 还没有设置代码根目录,请参考脚本说明设置代码根目录."
+        cdcode_show_help
+    else
+        # 如果 project_root_dir 变量不为空,这不带参数时,默认cd到代码根目录下
+        \cd "${project_root_dir}"
+    fi
+    # 由于不带参数,不需要往下处理,直接return
+    return 0
 fi
 
 # 经过验证发现,用source命令来执行该脚本后,OPTIND的值不会被重置为1,导致
 # 再次用source命令执行脚本时,getopts解析不到参数,所以手动重置OPTIND为1.
 OPTIND=1
-while getopts "hfpr:snmxglvi:ea:d:" opt; do
+while getopts "hf:pr:smlvi:ea:d:" opt; do
     # 调用parsecfg.sh脚本处理选项的函数来处理配置文件相关的选项.
     # 如果处理成功,就直接继续读取下一个选项,不再往下处理.
     # handle_config_option()函数要求传入的选项以'-'开头,而getopts命令
@@ -336,14 +254,11 @@ while getopts "hfpr:snmxglvi:ea:d:" opt; do
 
     case $opt in
         h) cdcode_show_help ;;
-        f) search_files=1 ;;
+        f) godir "$OPTARG" ;;
         p) show_project_root ;;
         r) setup_project_root "$OPTARG" ;;
-        s) source_and_lunch "${BUILD_ENG}" ;;
-        n) compile_all ;;
+        s) source_and_lunch "${DEFAULT_LAUNCH_COMBO}" ;;
         m) compile_android ;;
-        x) compile_lichee ;;
-        g) compile_gms ;;
         ?) cdcode_show_help ;;
     esac
 done
@@ -353,11 +268,17 @@ if [ $# -eq $((OPTIND-1)) ]; then
     return 0
 fi
 
-# 尝试更新当前代码根目录,具体信息如该函数的注释所示.
-update_project_root
+# 检测当前路径是否位于代码根目录下,如果不是,打印一些提示信息.
+check_project_root
 
 # 移动脚本的参数,去掉前面输入的选项,只剩下表示路径简写的参数.
 shift $((OPTIND-1))
+# 循环遍历所有剩余的参数,可以处理传入多个参数的情况.
+# 例如执行 cdcode.sh -r rootdir a 命令,可以先设置代码根目录,
+# 并cd到a简写对应的目录下.执行cdcode.sh -r rootdir -s +a 命令
+# 就会设置代码根目录,并进行source,然后到a简写对应的目录下编译.
+# 这种情况要注意提供的参数顺序,如果写为cdcode.sh a -r rootdir,
+# 会在解析a简写时发现还没有设置代码根目录导致报错.
 for arg in "$@"; do
     handle_symbol "$arg"
 done
