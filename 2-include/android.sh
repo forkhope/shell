68a11f6162f049a7a9df4a8b7da50980b6373e43
L <karmameet@gmail.com>
2019-08-08 10:57:52 +0800

    Update android.sh
    移除特殊环境、特殊目录的限制

--- 2-include/android.sh
+++ 2-include/android.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-# 定义 Android 相关脚本的一些通用常量.
-EXDROID="exdroid"
-LICHEE="lichee"
+FRAMEWORKS="frameworks"
+
+# 该函数检查当前工作目录是否存在一个指定的子目录.
+# $1 -- 要检查的子目录名
+# 如果 $1 不是当前工作目录的子目录,函数会报错返回.
+pwd_check_sub_dir()
+{
+    if [ $# -ne 1 ]; then
+        echo "Usage: $FUNCNAME dirname"
+        return 1
+    fi
+    local dir_to_check
+
+    dir_to_check="$1"
+    if [ ! -d "${dir_to_check}" ]; then
+        echo "当前目录 $(pwd) 下不包含指定的 ${dir_to_check} 目录."
+        echo "请 cd 到包含 ${dir_to_check} 的目录!"
+        return 1
+    fi
+}
